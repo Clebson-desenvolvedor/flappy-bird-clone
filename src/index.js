c07642b6f4fcbcb866831a956d1e576178e271a2
clebson.araujo <clebson.araujo@l5.com.br>
2025-02-01 23:12:43 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import Phaser from "phaser";
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: "arcade",
+    },
+    scene: {
+        preload,
+        create
+    }
+}
+
+function preload() {
+    this.load.image("sky", "assets/sky.png");
+    this.load.image("bird", "assets/bird.png");
+}
+
+let bird = null;
+
+function create() {
+    this.add.image(0, 0, "sky").setOrigin(0, 0);
+    bird = this.add.sprite(config.width * 0.1, config.height / 2, "bird").setOrigin(0);
+    debugger
+}
+
+new Phaser.Game(config);
